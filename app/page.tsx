a186515785c9b9d1959fab27700d763cf24815d3
pawelblaszczyk5 <internal@pawelblaszczyk.dev>
2024-01-04 21:51:51 +0100

    Some random changes

--- app/page.tsx
+++ app/page.tsx
@@ -4,15 +4,39 @@ import { Suspense } from "react";
 const ExampleStreamedContent = async () => {
   unstable_noStore();
 
-  await new Promise((res) => setTimeout(res, 1500));
+  await new Promise((res) => setTimeout(res, 500));
 
   return (
     <>
       <span>{Math.random()}</span>
-      <img src="https://picsum.photos/200/300" width={200} height={300} />
-      <img src="https://picsum.photos/220/300" width={220} height={300} />
-      <img src="https://picsum.photos/230/300" width={230} height={300} />
-      <img src="https://picsum.photos/240/300" width={240} height={300} />
+      <img
+        className="bg-sky-900"
+        src="https://picsum.photos/200/300"
+        alt=""
+        width={200}
+        height={300}
+      />
+      <img
+        className="bg-sky-900"
+        src="https://picsum.photos/220/300"
+        alt=""
+        width={220}
+        height={300}
+      />
+      <img
+        className="bg-sky-900"
+        src="https://picsum.photos/230/300"
+        alt=""
+        width={230}
+        height={300}
+      />
+      <img
+        className="bg-sky-900"
+        src="https://picsum.photos/240/300"
+        alt=""
+        width={240}
+        height={300}
+      />
     </>
   );
 };
